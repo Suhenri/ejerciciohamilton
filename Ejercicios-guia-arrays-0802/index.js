3d6aae589823f70a802e151797e913b0a6508d41
Suhenri <suhenri.a@gmail.com>
2023-02-10 16:08:54 -0300

    first commit

---
+++ Ejercicios-guia-arrays-0802/index.js
@@ -0,0 +1,22 @@
+/* Escriba un programa que permita imprimir en pantalla cada uno de los elementos del siguiente
+arreglo: [0, 28, 30, 10, 4].
+*/
+// 1° se crea el array con la variable let
+let arrayPrimerEjercicio = [0, 28, 30, 10, 4];
+
+/* 2° después esa variable se llama con el método forEach y la función se reemplaza con un array function (element =>)
+
+arrayPrimerEjercicio.forEach(element => console.table(element));
+
+/* Escriba un programa que permita imprimir al revés cada uno de los elementos del siguiente
+arreglo: [0, 28, 30, 10, 4].
+*/
+let arraySegundoEjercicio = ['uno', 'dos', 'tres'];
+console.log(arraySegundoEjercicio);
+let reversed = arraySegundoEjercicio.reverse();
+console.log(reversed);
+arraySegundoEjercicio.forEach(element => console.table(element));
+
+/* Escriba un programa que permita calcular la suma de todos los elementos del siguiente arreglo:
+[1, 3, 6, 82, 23].
+*/
